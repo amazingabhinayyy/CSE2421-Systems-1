a023a2c87694f2b26b2312f167262f4fa2b1d56c
amazingabhinayyy <51940031+amazingabhinayyy@users.noreply.github.com>
2023-02-10 17:47:44 -0500

    Message

---
+++ 3 HelloWorld + General C/HelloWorld2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (void)
+{
+    printf("Hello, World!!!\n ");
+    int y = 12;
+    y++;
+    return 0;
+}
---
+++ General.c
@@ -0,0 +1,8 @@
+/* main methods
+
+void main (int argc, char **argv) - gives errors in VSCode
+after running on generated file you can pass in arguments by doing./name arg1 arg2 aeg 3
+
+
+
+*/
